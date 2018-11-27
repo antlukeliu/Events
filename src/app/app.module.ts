6adc69b248ccd8921eef6fb53645fce9b0a6f83c
Anthony Liu <antmliu@gmail.com>
2018-11-26 23:30:31 -0500

    Adding Fundamentals of Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,63 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {
+  EventsListComponent,
+  EventThumbnailComponent,
+  EventService,
+  EventDetailsComponent,
+  CreateEventComponent,
+  EventRouteActivator,
+  EventListResolver,
+  CreateSessionComponent,
+  SessionListComponent,
+  DurationPipe
+} from './events/index'
+import { EventsAppComponent } from './events-app.component';
+import { NavBarComponent } from './nav/navbar.component';
+import { ToastrService } from './common/toastr.service';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './routes';
+import { Error404Component } from './errors/404.component';
+import { AuthService } from './user/auth.service';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { CollapsibleWellComponent } from './common/collapsible-well.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    Error404Component,
+    CreateEventComponent,
+    EventsAppComponent,
+    EventDetailsComponent,
+    EventsListComponent,
+    EventThumbnailComponent,
+    NavBarComponent,
+    CreateSessionComponent,
+    SessionListComponent,
+    CollapsibleWellComponent,
+    DurationPipe
+  ],
+  providers: [
+    EventService,
+    ToastrService,
+    EventRouteActivator,
+    EventListResolver,
+    AuthService,
+    { provide: 'canDeactivateCreateEvent' , useValue: checkDirtyState }
+  ],
+  bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
+
+export function checkDirtyState(component: CreateEventComponent) {
+  if (component.isDirty) {
+    return window.confirm('You have not saved this event, do you really want to cancel?');
+  }
+    return true;
+}
