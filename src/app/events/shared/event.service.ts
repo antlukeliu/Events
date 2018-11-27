6d5c8772d96971f584e17dc64bf904bbe433ec5f
Anthony Liu <antmliu@gmail.com>
2018-11-27 09:13:06 -0500

    Adding session search

--- src/app/events/shared/event.service.ts
+++ src/app/events/shared/event.service.ts
@@ -1,7 +1,6 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
 import { Subject, Observable } from 'rxjs';
-import { IEvent } from './event.model';
-
+import { IEvent, ISession } from './event.model';
 
 export class EventService {
   getEvents():Observable<IEvent[]> {
@@ -22,8 +21,28 @@ export class EventService {
   }
 
   updateEvent(event) {
-    let index = EVENTS.findIndex(x => x.id = event.id);
+    const index = EVENTS.findIndex(x => x.id = event.id);
     EVENTS[index] = event;
+  }
+
+  searchSessions(searchTerm: string){
+    const term = searchTerm.toLocaleLowerCase();
+    let results: ISession[] = [];
+
+    EVENTS.forEach(event => {
+      let matchingSessions = event.sessions.filter(session => 
+        session.name.toLocaleLowerCase().indexOf(term) > -1);
+      matchingSessions = matchingSessions.map((session:any) => {
+        session.eventId = event.id;
+        return session;
+      })
+      results = results.concat(matchingSessions);
+    })
+    const emitter = new EventEmitter(true);
+    setTimeout(() => {
+      emitter.emit(results);
+    }, 100);
+    return emitter;
   }
 }
 
